f5c8aa7ea19f8f9e1079fcf426dccf6b0d882859
Ali-2001-Eg <AliBusiness2001@outlook.com>
2023-11-14 17:48:15 +0200

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,14 @@ import 'package:faenonibeqwa/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
+  Future.wait([
+    Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform),
+  ]);
   runApp(const ProviderScope(child: MyApp()));
 }
 
