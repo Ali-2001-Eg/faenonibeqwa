6cd53e30a2a8f3885afc8221f12fd880a2ef1cb1
Alimazen-2001 <alimazen2001ali@gmail.com>
2024-03-04 16:24:10 +0200

    push

--- lib/main.dart
+++ lib/main.dart
@@ -79,7 +79,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    FirebaseAuth.instance.signOut();
+    // FirebaseAuth.instance.signOut();
     return ScreenUtilInit(
         designSize: const Size(360, 690),
         minTextAdapt: true,
